ac4ca91da990820c42bcbd29d05c2c2496fcdc0e
carlos <cadu.lp@gmail.com>
2023-04-20 20:55:15 -0300

    Update

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 sudo su <<HERE
-cp /opt/tomcat/latest/webapps/hello-world.war /home/carlos/work/primeup/arbi/repos/tomcat-sample-app/tomcat/backup/hello-world.war
+cd /
+cp /opt/tomcat/latest/webapps/hello-world.war /backup/
+ls backup/
 HERE
 
