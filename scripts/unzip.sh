8e0fbf6945e89360e001c1e1de363b8f232b45bd
carlos <cadu.lp@gmail.com>
2023-04-24 14:43:34 -0300

    test

--- scripts/unzip.sh
+++ scripts/unzip.sh
@@ -6,10 +6,8 @@ cd /
 cp backup/*.war work/
 unzip /work/*.war "META-INF/*" -d "/work/" 
 unzip /war/*.war -d "/war/"
-ls /war/
 mv /work/META-INF/context.xml /war/META-INF/
 cd war
-ls
 zip -r -u hello-world.war META-INF
 HERE
 
