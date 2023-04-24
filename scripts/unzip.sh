a876b1e649cd969050c4e2e1587e0b0d9482ef62
carlos <cadu.lp@gmail.com>
2023-04-24 11:12:55 -0300

    Undeploy

--- scripts/unzip.sh
+++ scripts/unzip.sh
@@ -2,10 +2,10 @@
 
 sudo su <<HERE
 cd /
-mkdir work
 cp backup/*.war work/
 unzip /work/*.war "META-INF/*" -d "/war/"
 unzip /home/ubuntu/actions-runner/tomcat/tomcat/tomcat/war/hello-world.war "META-INF/*" -d "/home/ubuntu/actions-runner/tomcat/tomcat/tomcat/war/"
+pwd
 cp /work/META-INF/context.xml home/ubuntu/actions-runner/tomcat/tomcat/tomcat/war/META-INF
 cd home/ubuntu/actions-runner/tomcat/tomcat/tomcat/war/
 zip -r -u hello-world.war META-INF
