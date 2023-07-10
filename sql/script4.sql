8c2ae8da032d331940c04b076380cd7be1286b59
carlos <cadu.lp@gmail.com>
2023-07-10 11:45:25 -0300

    Update files - 2023-07-10T14:45:24.934Z

---
+++ sql/script4.sql
@@ -0,0 +1,14 @@
+USE BancoDeDadosTomCat;
+GO
+
+CREATE TABLE TB_TOMCAT004 (
+    ID INT IDENTITY(1,1) PRIMARY KEY,
+    Nome VARCHAR(50),
+    Sobrenome VARCHAR(50),
+    Idade INT,
+    Email VARCHAR(100)
+    );
+GO
+
+
+
